4106298ddbb3e6537aa908c9dbe77ccaf3d82c15
Milan Grdosic <milangrdosic2468@gmail.com>
2024-08-27 13:39:09 +0930

    S

--- week5/Multiples/Multiples.hpp
+++ week5/Multiples/Multiples.hpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Multiples {
     public:
-        int numbers(signed long long int min, signed long long int max, int factor) {
+        int number(signed long long int min, signed long long int max, int factor) {
             int count = 0;
             for(auto i = min; i <= max; i++) {
                 if ( i % factor == 0) {
