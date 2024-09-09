56e5b0610d8919711131c5b8299aca12bcdc326c
Milan Grdosic <milangrdosic2468@gmail.com>
2024-09-09 22:58:01 +0930

    S

--- week6/NiceOrUgly/NiceOrUgly.hpp
+++ week6/NiceOrUgly/NiceOrUgly.hpp
@@ -1,64 +1,91 @@
 #include <iostream>
-#include <stdlib.h>
+#include <string>
 
 using namespace std;
 
 class NiceOrUgly {
-    public:
-        string describe(string s) {
-            bool ugly = false, canBeNice = true;
-            int size = s.length();
+public:
+    string describe(string s) {
+        string allVowels = replaceQuestionMarks(s, true);
+        string allConsonants = replaceQuestionMarks(s, false);
+        string adaptiveReplace = adaptiveQuestionMarks(s);
 
-            int totalCases = 1 << count(s.begin(), s.end(), '?');
-            for (int mask = 0; mask < totalCases; mask++) {
-                string testStr = s;
-                int questionIndex = 0;
-                for (int i = 0; i < size; i++) {
-                    if (s[i] == '?') {
-                        if (mask & (1 << questionIndex)) {
-                            testStr[i] = 'O'; 
-                        } else {
-                            testStr[i] = 'Z'; 
-                        }
-                        questionIndex++;
-                    }
-                }
-                if (isUgly(testStr)) {
-                    ugly = true;
-                } else {
-                    canBeNice = false; 
-                }
-            }
+        bool isAllVowelsUgly = isUgly(allVowels);
+        bool isAllConsonantsUgly = isUgly(allConsonants);
+        bool isAdaptiveUgly = isUgly(adaptiveReplace);
 
-            if (ugly && !canBeNice) {
-                return "UGLY";
-            } else if (!ugly && canBeNice) {
-                return "NICE";
-            } else {
-                return "42"; 
+        if (isAllVowelsUgly && isAllConsonantsUgly && isAdaptiveUgly) {
+            return "UGLY";
+        } else if (!isAllVowelsUgly && !isAllConsonantsUgly && !isAdaptiveUgly) {
+            return "NICE";
+        } else {
+            return "42";
+        }
+    }
+
+private:
+    // Replace all '?' with vowels or consonants uniformly
+    string replaceQuestionMarks(const string& s, bool useVowel) {
+        string result = s;
+        for (char& c : result) {
+            if (c == '?') {
+                c = useVowel ? 'A' : 'Z';
             }
         }
+        return result;
+    }
 
-    private:
-        bool isUgly(const string& str) {
-            int vowelCount = 0;
-            int consonantCount = 0;
-            for (char ch : str) {
-                if (isVowel(ch)) {
+    // Adaptively replace '?' based on the context to prevent sequences of vowels or consonants
+    string adaptiveQuestionMarks(string s) {
+        int vowelCount = 0, consonantCount = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '?') {
+                if (vowelCount >= 2) { // already 2 vowels, place a consonant
+                    s[i] = 'Z';
+                    vowelCount = 0;
+                    consonantCount = 1;
+                } else if (consonantCount >= 4) { // already 4 consonants, place a vowel
+                    s[i] = 'A';
+                    consonantCount = 0;
+                    vowelCount = 1;
+                } else {
+                    // place a vowel by default if not in danger of becoming ugly
+                    s[i] = 'A';
+                    vowelCount++;
+                    consonantCount = 0;
+                }
+            } else {
+                if (isVowel(s[i])) {
                     vowelCount++;
                     consonantCount = 0;
                 } else {
                     consonantCount++;
                     vowelCount = 0;
                 }
-                if (vowelCount == 3 || consonantCount == 5) {
-                    return true;
-                }
             }
-            return false;
         }
+        return s;
+    }
 
-        bool isVowel(char c) {
-            return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    bool isUgly(string str) {
+        int vowelCount = 0, consonantCount = 0;
+        for (char ch : str) {
+            if (isVowel(ch)) {
+                vowelCount++;
+                consonantCount = 0;
+            } else {
+                consonantCount++;
+                vowelCount = 0;
+            }
+            if (vowelCount == 3 || consonantCount == 5) {
+                return true;
+            }
         }
+        return false;
+    }
+
+    bool isVowel(char c) {
+        return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
 };
+
